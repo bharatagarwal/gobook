82f930ce219beefa800696482762c13395279612
Bharat Agarwal <bharatagarwal@gmail.com>
2021-05-15 11:18:19 +0530

    Refactoring dup2 to have a shorter main function

--- pkg/ch1/dup2/main.go
+++ pkg/ch1/dup2/main.go
@@ -13,22 +13,10 @@ func main() {
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
+		processFiles(files, counts)
 	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+	printDuplicates(counts)
 }
 
 func countLines(f *os.File, counts map[string]int) {
@@ -36,5 +24,27 @@ func countLines(f *os.File, counts map[string]int) {
 
 	for input.Scan() {
 		counts[input.Text()] += 1
+	}
+}
+
+func processFiles(files []string, counts map[string]int) {
+	for _, arg := range files {
+		f, err := os.Open(arg)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+
+		countLines(f, counts)
+		f.Close()
+	}	
+}
+
+func printDuplicates(counts map[string]int) {
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
 	}
 }
